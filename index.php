4e4afdc603490bba3a0b5a473e40b1f12f472cd6
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-08-07 22:37:21 +0545

    working

--- index.php
+++ index.php
@@ -8,16 +8,21 @@ include("header.php");
         <li data-target="#carouselId" data-slide-to="0" class="active"></li>
         <li data-target="#carouselId" data-slide-to="1"></li>
         <li data-target="#carouselId" data-slide-to="2"></li>
+                <li data-target="#carouselId" data-slide-to="3"></li>
+
     </ol>
     <div class="carousel-inner" role="listbox">
         <div class="carousel-item active">
-            <img data-src="holder.js/900x500/auto/#777:#555/text:First slide" alt="First slide">
+            <img src="Images/banner1.jpeg" alt="First slide" style="width: 100%; height: 450px;">
         </div>
         <div class="carousel-item">
-            <img data-src="holder.js/900x500/auto/#666:#444/text:Second slide" alt="Second slide">
+            <img src="Images/banner2.jpeg" alt="Second slide" style="width: 100%; height: 450px;">
         </div>
         <div class="carousel-item">
-            <img data-src="holder.js/900x500/auto/#666:#444/text:Third slide" alt="Third slide">
+            <img src="Images/banner3.jpeg" alt="Third slide" style="width: 100%; height: 450px;">
+        </div>
+           <div class="carousel-item">
+            <img src="Images/banner4.jpeg" alt="Fourth slide" style="width: 100%; height: 450px;">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
