bdb622a608d15f98e606969c4e96435c841fbbe0
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-08-12 20:30:36 +0545

    busy

--- conn.php
+++ conn.php
@@ -1,13 +1,15 @@
 <?
-class con{
+class connec{
     public $username="root";
     public $password="";
     public $server_name="localhost";
     public $db_name="movie_ticket_booking";
 
+
+    public $conn;
     function __construct()
     {
-        
+        $this->conn = new mysqli($username, $password, $server_name, $db_name);
         
     }
 }
