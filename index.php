f739b8de75de255be8c06553aa28b86e2aa20204
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-08-17 17:55:27 +0545

    working

--- index.php
+++ index.php
@@ -3,16 +3,21 @@ include("header.php");
 include("conn.php");
 
 $conn = new connec();
-$data = $conn->select_all('slider');
+$tbl = "slider";
+$result = $conn->select_all($tbl);
+$result1 = $conn->select_all($tbl);
+
 ?>
 
 <section style="min-height: 450px;">
     <div id="carouselId" class="carousel slide" data-ride="carousel">
         <?php
-        if ($data->num_rows > 0) {
+        if ($result->num_rows > 0) {
+
             $i = 0;
             echo '<ol class="carousel-indicators">';
-            while ($row = $data->fetch_assoc()) {
+
+            while ($row = $result->fetch_assoc()) {
                 if ($i == 0) {
                     echo '<li data-target="#carouselId" data-slide-to="' . $i . '" class="active"></li>';
                 } else {
@@ -31,7 +36,33 @@ $data = $conn->select_all('slider');
 
     </ol> -->
         <div class="carousel-inner" role="listbox">
-            
+            <?php
+            if ($result1->num_rows > 0) {
+
+                $j = 0;
+                while ($row1 = $result1->fetch_assoc()) {
+
+                    if ($j == 0) {
+                        ?>
+                            <div class="carousel-item active">
+                                <img src="<?php echo $row1["img_path"]; ?>" alt="<?php echo $row1["alt"]; ?>" style="width: 100%; height: 500px;">
+                            </div>
+                        <?php
+                    } 
+                    else {
+                        ?>
+                            <div class="carousel-item">
+                                <img src="<?php echo $row1["img_path"]; ?>" alt="<?php echo $row1["alt"]; ?>" style="width: 100%; height: 500px;">
+                            </div>
+                        <?php
+                    }
+                       $j++;
+                }
+             
+                
+            }
+                ?>
+                <!-- 
              <div class="carousel-item active">
             <img src="Images/banner1.jpeg" alt="First slide" style="width: 100%; height: 500px;">
         </div>
@@ -44,17 +75,17 @@ $data = $conn->select_all('slider');
            <div class="carousel-item">
             <img src="Images/banner4.jpeg" alt="Fourth slide" style="width: 100%; height: 500px;">
         </div>
-         
+         -->
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
         </div>
-        <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
 
 
 
