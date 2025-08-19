4a23e268ed6690300b51a3a647477266e6599a86
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-08-19 19:37:45 +0545

    wroking

--- commingsoon.php
+++ commingsoon.php
@@ -5,6 +5,14 @@ include("conn.php");
 
 <section class="mt-5 ">
     <h5 class="text-center">Comming soon</h5>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                
+            </div>
+        </div>
+    </div>
 </section>
 <?php
 include("footer.php");
