abf44c3e5dcc36a4357ce5ce1a154bc044bdc89d
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-08-20 20:31:24 +0545

    Working

--- commingsoon.php
+++ commingsoon.php
@@ -3,6 +3,9 @@ include("header.php");
 include("conn.php");
 
 $conn = new connec();
+$tbl = "movie";
+$result = $conn->select_all($tbl);
+
 ?>
 
 <section class="mt-5 ">
@@ -10,14 +13,25 @@ $conn = new connec();
 
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-            <img src="" alt="" style="width: 100%; height: 250px;">
-            <h6 class="text-center mt-2">Movie Name</h6>
-            <p><b>Release Date:</b></p>
-            <p><b>Industry:</b></p>
-            <p><b>Language:</b></p>
-            <p><b>Genre:</b></p>
-            </div>
+            <?php
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+            ?>
+                    <div class="col-md-3">
+                        <img src="" alt="" style="width: 100%; height: 250px;">
+                        <h6 class="text-center mt-2">Movie Name</h6>
+                        <p><b>Release Date:</b></p>
+                        <p><b>Industry:</b></p>
+                        <p><b>Language:</b></p>
+                        <p><b>Genre:</b></p>
+                    </div>
+            <?php
+
+                }
+            }
+
+            ?>
+
         </div>
     </div>
 </section>
