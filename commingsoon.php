99326a287b86a5e2d5db9eadd735753afa9a4a10
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-08-20 20:54:06 +0545

    Working

--- commingsoon.php
+++ commingsoon.php
@@ -18,12 +18,12 @@ $result = $conn->select_all($tbl);
                 while ($row = $result->fetch_assoc()) {
             ?>
                     <div class="col-md-3">
-                        <img src="" alt="" style="width: 100%; height: 250px;">
-                        <h6 class="text-center mt-2">Movie Name</h6>
-                        <p><b>Release Date:</b></p>
-                        <p><b>Industry:</b></p>
-                        <p><b>Language:</b></p>
-                        <p><b>Genre:</b></p>
+                        <img src="" alt="" style="width: 100%; height: 250px;"/>
+                        <h6 class="text-center mt-2"><?php echo $row["name"];?></h6>
+                        <p><b>Release Date :</b><?php echo $row["rel_date"];?></p>
+                        <p><b>Industry:</b><?php echo $row["industry_id"];?></p>
+                        <p><b>Language:</b><?php echo $row["lang_id"];?></p>
+                        <p><b>Genre:</b><?php echo $row["genre_id"];?></p>
                     </div>
             <?php
 
