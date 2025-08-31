86ec5690bdc0724ef6fce231ff90989441e8b326
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-08-31 19:10:18 +0545

    working

--- about.php
+++ about.php
@@ -1,15 +1,10 @@
 <?php
 include("header.php");
-include("conn.php");
-
-$conn = new connec();
-$tbl = "movie";
-$result = $conn->select_movie($tbl, "now()");
 
 ?>
 
 <section class="mt-5 ">
-    <h3 class="text-center" style="color:darkcyan;">Comming soon</h3>
+    <h3 class="text-center" style="color:darkcyan;">About</h3>
 
 
 </section>
---
+++ booking.php
@@ -0,0 +1,13 @@
+<?php
+include("header.php");
+
+?>
+
+<section class="mt-5 ">
+    <h3 class="text-center" style="color:darkcyan;">Book Your Ticket Now</h3>
+
+
+</section>
+<?php
+include("footer.php");
+?>
