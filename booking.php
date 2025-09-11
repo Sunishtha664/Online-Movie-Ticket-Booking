eec2b9e78f8d8aca5781af9db170be4ed8a992bc
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-09-11 16:16:41 +0545

    working

--- booking.php
+++ booking.php
@@ -7,7 +7,27 @@ include("header.php");
     <h3 class="text-center" style="color:darkcyan;">Book Your Ticket Now</h3>
 
     <form action="" method="post" class="mt-5">
-        <div class="container"></div>
+        <div class="container" style="color:darkcyan;" >
+            <center>
+                <p style="color: black;">Please fill this form to book your ticket</p>
+            </center><hr>
+            <label for="username"><b>Username</b></label>
+            <input type="text" style="border: radius 30px;" placeholder="Enter Username" name="username" id="username" required>
+
+            <label for="email"><b>Email</b></label>
+            <input type="text" style="border: radius 30px;" placeholder="Enter Email" name="email" id="email" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="text" style="border: radius 30px;" placeholder="Enter Password" name="psw" id="psw" required>
+
+            <label for="psw-repeat"><b>Repeat Password</b></label>
+            <input type="text" style="border: radius 30px;" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+
+            <label for="number"><b>Number</b></label>
+            <input type="text" style="border: radius 30px;" placeholder="Enter Number" name="number" id="number" required>
+
+            <button type="submit" class="btn" style="background-color:darkcyan; color: white;"></button>
+        </div>
     </form>
 
 
