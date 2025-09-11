6b4a6f1153143ee654709b2caa8ab6cf495922ae
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-09-11 16:02:19 +0545

    working

--- booking.php
+++ booking.php
@@ -6,6 +6,10 @@ include("header.php");
 <section class="mt-5 ">
     <h3 class="text-center" style="color:darkcyan;">Book Your Ticket Now</h3>
 
+    <form action="" method="post" class="mt-5">
+        <div class="container"></div>
+    </form>
+
 
 </section>
 <?php
