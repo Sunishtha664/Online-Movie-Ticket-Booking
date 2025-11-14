07f5d5ff91cc58523e9737778230b1d2f335b026
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-11-14 21:12:37 +0545

    Working

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -1,11 +1,12 @@
 <?php 
 session_start();
-if(!empty($_SESSION["admin_username"])){
-header("location: index.php");
+
+if(empty($_SESSION["admin_username"])){
+    header("Location:index.php");
 }
 else{
-    ?>
-    <!doctype html>
+     ?>
+     <!doctype html>
 <html lang="en">
   <head>
     <title>Admin Panel - Online Movie Ticket</title>
@@ -46,12 +47,8 @@ else{
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
-
-
-
-
-
     <?php
 }
-
 ?>
+
+
