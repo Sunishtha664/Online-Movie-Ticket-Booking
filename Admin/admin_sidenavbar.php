ac4b65803f41a3ba4322d5b04083801db85f16cf
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-11-16 18:35:32 +0545

    working

---
+++ Admin/admin_sidenavbar.php
@@ -0,0 +1,8 @@
+<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+              
+
+                <li class="nav-item"><a class="nav-link" href="booking.php">Book Ticket</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+            </ul>
