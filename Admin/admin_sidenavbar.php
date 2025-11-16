41dcc0fcfde6fddc65e95b8e6e0cadab6d2b2168
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-11-16 18:50:15 +0545

    Working

--- Admin/admin_sidenavbar.php
+++ Admin/admin_sidenavbar.php
@@ -1,18 +1,48 @@
 <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ml-5">
     <li class="nav-item">
-        <a class="nav-link" href="index.php" style="color: white;">Home</a>
+        <a class="nav-link" href="#" style="color: white;">Movie Booking</a>
      </li>
 
 
     <li class="nav-item">
-        <a class="nav-link" href="booking.php" style="color: white;">Book Ticket</a>
+        <a class="nav-link" href="#" style="color: white;">Cinema</a>
     </li>
 
     <li class="nav-item">
-        <a class="nav-link" href="about.php" style="color: white;">About</a>
+        <a class="nav-link" href="#" style="color: white;">Contact</a>
     </li>
 
     <li class="nav-item">
-        <a class="nav-link" href="contact.php" style="color: white;">Contact</a>
+        <a class="nav-link" href="#" style="color: white;">Customer</a>
     </li>
+
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Movie Genre</a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Movie Industry</a>
+    </li>
+
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Movie Language</a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Movie</a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Seat Details</a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Seat Reserved</a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Movie Show</a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Movie Show Time</a>
+    </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;">Slider</a>
+    </li>
+
 </ul>
