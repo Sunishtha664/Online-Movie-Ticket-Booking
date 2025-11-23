78ded8019395be8fe6e3752d11cf9e5102341985
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-11-23 19:36:11 +0545

    admin work

--- Admin/index.php
+++ Admin/index.php
@@ -2,7 +2,7 @@
 session_start();
 $error = "";
 
-// If already logged indvjdnvk
+// If already logged in
 if (!empty($_SESSION["admin_username"])) {
     header("Location: dashboard.php");
     exit();
