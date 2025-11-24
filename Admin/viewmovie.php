1eb9ff046eb729700b95a06481baf824dcecba0d
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-11-24 17:51:32 +0545

    working

--- Admin/viewmovie.php
+++ Admin/viewmovie.php
@@ -16,7 +16,31 @@ if (empty($_SESSION["admin_username"])) {
                     <?php include('admin_sidenavbar.php'); ?>
                 </div>
                 <div class="col-md-10">
-                    <h5 class="text-center mt-2" style="color:maroon;">View Movie</h5>
+                    <h5 class="text-center mt-2" style="color:maroon;">Movie Details </h5>
+                    <a href="addmovie.php">Add Movie</a>
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row"></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td scope="row"></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    
                 </div>
             </div>
         </div>
