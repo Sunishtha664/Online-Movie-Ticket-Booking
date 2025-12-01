92a4eb000a6c7a647d3512db65ea982d36023534
Sunishtha664 <sunishthatuladhar22@gmail.com>
2025-12-01 20:54:54 +0545

    working still

--- Admin/viewmovie.php
+++ Admin/viewmovie.php
@@ -54,8 +54,8 @@ if (empty($_SESSION["admin_username"])) {
                                         <td></td>
                                         <td></td>
                                         <td></td>
-                                        <td><a href="editmovie.php?id=">Edit</a>
-                                            <a href="deletemovie.php?id=">Delete</a>
+                                        <td><a href="editmovie.php?id=">Edit</a> |
+                                            <a href="deletemovie.php?id=" style="color: red;">Delete</a>
                                         </td>
                                     </tr>
                             <?php
