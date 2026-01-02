89267e1a6368df0060a3a2f5c711984e4555a44a
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-01-02 11:57:55 +0545

    admin panel

--- Admin/admin_sidenavbar.php
+++ Admin/admin_sidenavbar.php
@@ -81,7 +81,7 @@
         <a class="nav-link" href="#" style="color: white;">Movie Show</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="#" style="color: white;">Movie Show Time</a>
+        <a class="nav-link" href="viewshowtime.php" style="color: white;">Movie Show Time</a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="viewslider.php" style="color: white;">Slider</a>
