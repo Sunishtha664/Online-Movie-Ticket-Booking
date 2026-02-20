4e1392f5581c8f42bac576397e5935e690e0111c
Sunishtha664 <sunishthatuladhar22@gmail.com>
2026-02-20 11:23:04 +0545

    defense

--- Admin/admin_sidenavbar.php
+++ Admin/admin_sidenavbar.php
@@ -48,14 +48,10 @@
         <a class="nav-link" href="viewbooking.php" style="color: white;">Movie Booking</a>
     </li>
 
-    <?php if (isset(
-        
-        
-        $_SESSION['admin_cinema_id']) && $_SESSION['admin_cinema_id'] == 0): ?>
+    <?php if (empty($_SESSION['admin_cinema_id']) || $_SESSION['admin_cinema_id'] == 0): ?>
     <li class="nav-item">
         <a class="nav-link" href="viewadmin.php" style="color: white;">Admins</a>
     </li>
-    <?php endif; ?>
 
     <li class="nav-item">
         <a class="nav-link" href="viewcinema.php" style="color: white;">Cinema</a>
@@ -71,6 +67,7 @@
     <li class="nav-item">
         <a class="nav-link" href="viewfeedback.php" style="color: white;">Feedback</a>
     </li>
+    <?php endif; ?>
 
     <li class="nav-item">
         <a class="nav-link" href="viewgenre.php" style="color: white;">Movie Genre</a>
